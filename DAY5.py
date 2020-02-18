9e9df58c05850693331ee0a1e1f07e6fea5b12a3
Prince Sharma <princesharma2899@gmail.com>
2020-02-18 06:23:35 +0530

    DAY 5 Modified

--- DAY5.py
+++ DAY5.py
@@ -1,16 +1,14 @@
+#https://www.hackerrank.com/challenges/array-left-rotation/problem
 N = input().split()
 input2 = input().split()
 arr = [None for i in range(int(N[0]))]
+get_arr = [None for i in range(int(N[0]))]
 def leftRotate(arr1, d, n):
-    for i in range(d): 
-        leftRotatebyOne(arr1, n)
-def leftRotatebyOne(arr2, n): 
-        temp = arr2[0]
-        for j in range(n-1):
-            arr2[j] = arr2[j+1]
-        arr2[n-1] = temp
-for i in range(0, int(N[0])):
+    global get_arr
+    for i in range(n): 
+        get_arr[(i+n-d)%n]=arr1[i] # d left rotation of n sized array = n-d right rotation of the array
+for i in range(0, int(N[0])): # quick way to left rotate a number of times and assign the array simultaneosly 
     arr[i] = int(input2[i])
 leftRotate(arr, int(N[1]), len(arr))
 for i in range(0, len(arr)):
-    print(arr[i], end=" ")
+    print(get_arr[i], end=" ")
